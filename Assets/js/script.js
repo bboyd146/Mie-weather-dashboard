07df265a61844a2d18732713ab27395342506072
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-06-28 23:43:32 -0500

    added event listener for button click

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,4 @@
+// Current weather values
 var tempVal = $('#temp');
 var windVal = $('#wind');
 var humidVal = $('#humidity');
@@ -6,12 +7,17 @@ var uvVal = $('#uv-index');
 // Weather API
 var weatherKey = "9ae9bb9e134c118baf51a3e5e47e8a55";
 var city = "houston";
-var weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + weatherKey;
 
-fetch(weatherAPI)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    }) 
+$('button').on('click', function(event) {
+    // text value for city search
+     var citySer = $("#city-search").val();
+    var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${citySer}&units=imperial&appid=${weatherKey}`;
+event.preventDefault();
+    fetch(weatherAPI)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        }) 
+})
